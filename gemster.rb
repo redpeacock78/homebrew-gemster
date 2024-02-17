c4af57a6ff257b167a9d66afc6ded143c457c3c4
redpeacock78 <webmaster@redpeacock78.com>
2024-02-17 20:37:34 +0900

    Update gemster.rb

--- gemster.rb
+++ gemster.rb
@@ -2,7 +2,7 @@ class Gemster < Formula
   desc "Simple task runner with file watch & hot reload functions"
   homepage "https://github.com/redpeacock78/gemster"
   url "https://ghrl.deno.dev/redpeacock78/gemster/gemster-macos.zip"
-  version "0.1.0"
+  version "0.1.1"
   sha256 "2ee743cbc05b85ce368fdf83c7c6d2fd3d9873fd5cfdb09ed9c7c45f0794665d"
   license "MIT"
 
