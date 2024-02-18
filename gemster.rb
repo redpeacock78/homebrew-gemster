5de7bad2b827ba8e90b1c280857610e8081d1e71
redpeacock78 <webmaster@redpeacock78.com>
2024-02-18 23:38:43 +0900

    gemster 0.1.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- gemster.rb
+++ gemster.rb
@@ -1,8 +1,8 @@
 class Gemster < Formula
   desc "Simple task runner with file watch & hot reload functions"
   homepage "https://github.com/redpeacock78/gemster"
-  url "https://github.com/redpeacock78/gemster/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "4d683429214d586986776e06dd0e486c98f5d4f925aadb3b64f748a7784ed29d"
+  url "https://github.com/redpeacock78/gemster/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "c0fdfeb8fc3dc287a30ea549cfb99af9cf29532cb32c76c266282ba71efbeeef"
   license "MIT"
 
   depends_on "crystal" => :build
